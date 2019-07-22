a4c64057a1737707c80b10a9b5980ef2275ea7e9
mike chui <mikel.k.khui@gmail.com>
2019-07-22 21:36:44 +0000

    Changed code to Template Literal

--- lotide/assertEqual.js
+++ lotide/assertEqual.js
@@ -1,11 +1,34 @@
-// FUNCTION IMPLEMENTATION
+/* FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
+  
   if(actual === expected) {
     console.log("Assertion Passed: [" + actual + "] === [" + expected + "]");//Assertion Passed: [actual] === [expected] (but with the values filled in)
   } else {
-    console.log("Assertion Failed: [" + actual + "] === [" + expected + "]");
+    console.log("Assertion Failed: [" + actual + "] !== [" + expected + "]");
     //Assertion Failed: [actual] !== [expected] (but with the values filled in)
   }
+};*/
+
+// Use Template Literals
+const assertEqual = function(actual, expected) {
+  const passStr = "Assertion Passed: ";
+  const failStr = "Assertion Failed: ";
+  const pass = " === ";
+  const fail = " !== ";
+  
+  let str = "";
+  let cond = "";
+
+  if(actual === expected) {
+    str = passStr;
+    cond = pass;
+    //Assertion Passed: [actual] === [expected] (but with the values filled in)
+  } else {
+    str = failStr;
+    cond = fail;
+    //Assertion Failed: [actual] !== [expected] (but with the values filled in)
+  }
+  console.log(`${str} [${actual}]${cond}[${expected}]`);
 };
 
 
